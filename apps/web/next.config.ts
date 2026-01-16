9f9b95009dd28d041336bd32f5a9a44f94e397f8
OluwapelumiG <ainaoluwapelumigideon@gmail.com>
2026-01-16 23:52:39 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@illtip-monorepo/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
